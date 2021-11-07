28a117b605e45aa71d90715b794857fb32485f7c
13657290358 <JaydenCheng@whut.edu.cn>
2021-11-07 16:07:12 +0800

    完善输出信息，便于数据核对。

--- function.py
+++ function.py
@@ -1,16 +1,17 @@
 import re
 DEFAULT = ""
-tooBig = []
-input_file = "./ref_info.html"
+tooBig = set()
+noData = set()
+input_file = "./original_data.html"
 
 def main(file_name):
     es_dict = read_info(file_name)  # 输入文件，解析文件，转换为字典{文章标题：[引用,...]}
     for title in es_dict:
         es_dict[title] = clear_info(es_dict[title])  # 输入文章字典，返回新文章字典，{文章标题：引用}
-    result_name = "download_1-307.txt"
+    result_name = "./download_1-414_converted.txt"
     print(write_file(result_name, es_dict))  # 将引用的部分写入目标文件
-    print(tooBig)
-
+    print("需要翻页的文章有：\n" + '\n'.join(list(tooBig)) + "\n一共有" + str(len(tooBig)) + "条。\n" + "\n没有引用的文章有：\n" + '\n'.join(list(noData)) + "\n一共有" + str(len(noData)) + "条。")
+    
 
 def read_info(file_name):
     infos = open(file_name, 'r', encoding="utf-8")
@@ -21,10 +22,13 @@ def read_info(file_name):
         title = re.findall(r"<title>.*?</title>", essay, re.S)
         if title == []:
             continue
-        title = title[0].replace("<title>", "").replace("</title>", "")
-        isBig = re.findall(r"共\d*?页",essay)
-        if isBig :
-            tooBig.append(title)
+        title = title[0].replace("<title>", "").replace("</title>", "").strip()
+        isBig = re.findall(r"共\d*?页", essay)
+        if isBig:
+            tooBig.add(title)
+            continue
+        if "号京公网安备" in essay:
+            noData.add(title)
             continue
         refs = re.findall(r"<ref>.*?</ref>", essay, re.S)[0]
         refs = refs.replace("<ref>", "").replace("</ref>", "").split('\n')
@@ -45,18 +49,18 @@ def clear_info(refs):
     book = tran_book(refs[-1])  # 获取参考图书列表，同上
     '''
     cleaned_refs = []
-    for ref in refs :
-        if "[M]" in ref :
+    for ref in refs:
+        if "[M]" in ref:
             ref = tran_book(ref)
-            if ref == None :
+            if ref == None:
                 continue
             cleaned_refs.append(','.join(ref))
-        elif ref.count(".") >= 3 :
+        elif ref.count(".") >= 3:
             ref = tran_ref(ref)
-            if ref == None :
+            if ref == None:
                 continue
             cleaned_refs.append(','.join(ref))
-        else :
+        else:
             continue
     # ref.extend(cleaned_refs)
     return cleaned_refs
@@ -71,7 +75,7 @@ def default_assignment(arr ,index):
 '''
 
 
-def tran_ref(ref): # 返回调整后的字符串。
+def tran_ref(ref):  # 返回调整后的字符串。
     # eg: [2]艺术感觉与美育. 滕守尧译,[美]拉尔夫.史密斯著. 四川人民出版社 . 1998
     '''
     result = []
@@ -88,6 +92,7 @@ def tran_ref(ref): # 返回调整后的字符串。
         # 滕守尧译,1998,四川人民出版社
         # result.append(','.join(ref))
     return ref
+
 
 
 def tran_book(book):
@@ -124,11 +129,12 @@ def tran_book(book):
 
 
 def write_file(citespace_file, essays_of_cnki):
+    count = 0
     file = open(citespace_file, 'r', encoding="utf-8")
     file = file.read()
     file = re.findall(r"PT.*?ER", file, re.S)
-    result = ""
     error = set()
+    result = ""
     for essay_in_origin in file:
         citespace_essay_title = re.findall(r"TI.*?SO", essay_in_origin, re.S)
         citespace_essay_title = citespace_essay_title[0].replace("TI", "").replace(
@@ -138,7 +144,7 @@ def write_file(citespace_file, essays_of_cnki):
             '\n', '').replace("AU", '').replace("AF", "").strip()
         for cnki_essay_title in essays_of_cnki:
             isFound = 0
-            if cnki_essay_title == citespace_essay_title:
+            if ((cnki_essay_title.replace(" ", '').replace("\n", '') in citespace_essay_title.replace(" ", '').replace("\n", '')) or (citespace_essay_title.replace(" ", '').replace("\n", '') in cnki_essay_title.replace("\n", '').replace(" ", ''))):
                 ref_list = []
                 #ref = '\n'.join(essays[t])
                 for ref in essays_of_cnki[cnki_essay_title]:  # 比对作者 剔除自引数据
@@ -146,6 +152,7 @@ def write_file(citespace_file, essays_of_cnki):
                         continue
                     else:
                         ref_list.append(ref)
+                count += len(ref_list)
                 ref = '\n'.join(ref_list)
                 #essay_in_file = re.sub(r"CR .*?NR","CR "+ref+"\nNR",essay_in_file ,re.S)
                 sub = re.findall(r"CR .*?NR", essay_in_origin, re.S)
@@ -153,15 +160,15 @@ def write_file(citespace_file, essays_of_cnki):
                     sub[0], "CR "+ref+"\nNR")
                 #del essays[t]
                 result = result + '\n' + essay_in_origin + '\n'
-                isFound = 1
-                if isFound:
-                    break
+                break
         else:
-            error.add(citespace_essay_title)
+           error.add(citespace_essay_title)
     file = open("reslt.txt", 'w', encoding="utf-8")
     file.write(result)
     file.close()
-    return "添加失败的文章有：" + str(error) + "，一共有" + str(len(error)) + "条。"
+    error = error - (tooBig | noData)
+    print(f"一共成功写入{count}篇")
+    return "标题有误：\n" + '\n'.join(list(error)) + "\n一共有" + str(len(error)) + "条。\n" 
 
 
 main(input_file)
