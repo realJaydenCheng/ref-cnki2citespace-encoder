91e5a9c7ef6ed805b6519528be1ba291eb4c2ca0
13657290358 <JaydenCheng@whut.edu.cn>
2021-11-07 17:16:47 +0800

    完善参数设置，增加获取效率

--- Auto_get.py
+++ Auto_get.py
@@ -6,38 +6,38 @@ import random
 # 保护措施，避免失控
 pyautogui.FAILSAFE = True
 # 为所有的PyAutoGUI函数增加延迟。默认延迟时间是0.1秒。
-pyautogui.PAUSE = 0.3
+pyautogui.PAUSE = 0.2
 
 notex, notey = 315, 1060
 
 # pyautogui.click(1805,15) # 最小化ide
 time.sleep(0.5)
 while 1:
+    re_num = 0
     try:
-        re_num = 0
-        pyautogui.click(100, 25, duration=0.5)
-        pyautogui.moveTo(760, 540, duration=0.5)
+        pyautogui.click(100, 25, duration=0.3)
+        pyautogui.moveTo(760, 540, duration=0.4)
         bt = pyautogui.locateOnScreen("./img/01sec_bt2.png")
         if bt == None:
             bt = pyautogui.locateOnScreen("./img/02sec_bt1.png")
         if bt == None:
-            pyautogui.scroll(-random.randint(100, 150))
+            pyautogui.scroll(-random.randint(400, 550))
             a = []
             re_num += 1
             print(f"未检测到可读取的文章，已重试{re_num}次。")
             a[100]
         (x, y) = pyautogui.center(bt)
-        pyautogui.moveTo(x, y, duration=0.5)
+        pyautogui.moveTo(x, y, duration=0.4)
         pyautogui.click()
-        pyautogui.moveTo(x+100, y, duration=0.3)
+        pyautogui.moveTo(x+100, y, duration=0.2)
         pyautogui.click()
-        time.sleep(19)
+        time.sleep(20)
         bt = pyautogui.locateOnScreen('./img/bigger.png')
         if bt == None:
             x, y = 760, 540
         else:
             (x, y) = pyautogui.center(bt)
-            pyautogui.moveTo(x, y, duration=0.5)
+            pyautogui.moveTo(x, y, duration=0.4)
             pyautogui.click()
         time.sleep(3)
         bt = pyautogui.locateOnScreen('./img/long.png')
@@ -45,19 +45,19 @@ while 1:
             x, y = 760, 540
         else:
             (x, y) = pyautogui.center(bt)
-            pyautogui.moveTo(x, y, duration=0.5)
+            pyautogui.moveTo(x, y, duration=0.3)
             pyautogui.click()
-        (x, y) = 1345, 335
-        pyautogui.moveTo(x, y, duration=0.5)
-        pyautogui.dragTo(x-1200, y, duration=0.8)
+        (x, y) = 1345, 330
+        pyautogui.moveTo(x, y, duration=0.3)
+        pyautogui.dragTo(x-1150, y, duration=0.55)
         pyautogui.hotkey('ctrl', 'c')
-        pyautogui.moveTo(notex, notey, duration=0.8)
+        pyautogui.moveTo(notex, notey, duration=0.3)
         pyautogui.click()
-        pyautogui.moveTo(1465, 1005, duration=0.3)
+        pyautogui.moveTo(1465, 1005, duration=0.2)
         pyautogui.typewrite("<p>\n<title>\n")
         pyautogui.hotkey("ctrl", 'v')
         pyautogui.typewrite("\n</title>\n")
-        pyautogui.moveTo(200, 300, duration=0.5)
+        pyautogui.moveTo(200, 300, duration=0.2)
         pyautogui.click()
         pyautogui.scroll(-random.randint(150, 180))
         #bt = locate_img.imgLocate('./img/04refs.png')
@@ -84,13 +84,13 @@ while 1:
             break
         pyautogui.hotkey("ctrl", 'a')
         pyautogui.hotkey('ctrl', 'c')
-        pyautogui.moveTo(notex, notey, duration=0.8)
+        pyautogui.moveTo(notex, notey, duration=0.2)
         pyautogui.click()
-        pyautogui.moveTo(1465, 1005, duration=0.3)
+        pyautogui.moveTo(1465, 1005, duration=0.2)
         pyautogui.typewrite("<ref>\n")
         pyautogui.hotkey("ctrl", 'v')
         pyautogui.typewrite("\n</ref>\n</p>\n\n")
-        pyautogui.moveTo(370, 25, duration=0.5)
+        pyautogui.moveTo(370, 25, duration=0.3)
         pyautogui.middleClick()
     except:
         continue
